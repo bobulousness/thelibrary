8b441ec0a23b4a31e5a5df407cf665c355c0da45
bobulousness <bobulousness@gmail.com>
2022-03-31 18:28:21 -0400

    added pages

---
+++ .idea/models/bookDB.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    name: String,
+    class: String,
+    requirements: String
+}, {collection: 'books'});
+
+const model = mongoose.model("books", bookSchema);
+
+module.exports.getBooks = async function (){
+    return await model.find();
+}
---
+++ .idea/src/main.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const books = require('./models/bookDB');
+var bodypars = require('body-parser');
+var session = require('express-session');
+
+
+main().catch(err => console.log(err));
+async function main(){
+
+
+    await mongoose.connect('mongodb://localhost:27017/Library',{useNewUrlParser: true, useUnifiedTopology: true});
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function () {});
+
+    var urle = bodypars.urlencoded({extended: false});
+    var app = express();
+    app.use(session({secret: 'hello'}));
+    app.set('view engine', 'ejs');
+
+    app.use('/assets', express.static('assets'));
+
+
+    var bookList = books.getBooks();
+
+    app.get('/', function (req, res) {
+        res.render('index', {bookList: bookList });
+    });
+
+
+
+
+
+}
+
+function setupDB(){
+    booksSet();
+}
+
+function booksSet(){
+
+}
