2eec3fd776351c2a293408d7fe9ab6594d68007a
bobulousness <bobulousness@gmail.com>
2022-03-31 21:41:55 -0400

    generates view port

---
+++ main.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const books = require('./models/bookDB');
+
+const game = new Phaser.Game(800,600,Phaser.AUTO,'game',
+    {preload:preload,create:create,update:update,render:render});
+
+async function preload(){
+    await mongoose.connect('mongodb://localhost:27017/Library',{useNewUrlParser: true, useUnifiedTopology: true});
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function () {});
+
+
+
+    var bookList = books.getBooks();
+}
+
+function create(){
+
+}
+
+function update(){
+
+}
+
+function render(){
+
+}
