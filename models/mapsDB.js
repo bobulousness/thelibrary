23778ab0a7f17a94e73c030f15a572126b71a5da
bobulousness <bobulousness@gmail.com>
2022-04-28 13:01:13 -0400

    rewrite

--- models/mapsDB.js
+++ models/mapsDB.js
@@ -1,33 +1,36 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const mapSchema = new mongoose.Schema({
     _id: {
         $oid: String
     },
+    name: String,
+    level: Number,
     coordinates:{
         start: {
             x: Number,
             y: Number,
-            z: Number
         },
         end:{
             x: Number,
             y: Number,
-            z: Number
         }
     },
     items: [{
             name: String,
             container: String,
             floor: Number,
-            state:String
+            hidden: Boolean,
+            known: Boolean
     }],
     creatures: [{
         1: {
             name: String,
             hp: Number,
             maxhp: Number,
-            floor: Number
+            floor: Number,
+            hidden: Boolean,
+            known: Boolean
         }
     }]
 }, {collection: 'maps'});
@@ -36,6 +39,21 @@ const model = mongoose.model("maps", mapSchema);
 
 module.exports.getMap = async function (){
     return await model.find();
+}
+
+module.exports.updateMap = async function (room){
+
+    var result = await model.findOne(room);
+
+    if (result) {
+        result.rsvp = rsvp;
+        result.save();
+        console.log("updated room");
+    } else {
+        let result = new model({user: user, con: con, rsvp: rsvp});
+        result.save();
+        console.log("saved new room");
+    }
 }
 
 
